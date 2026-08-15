4dfc3ce561b3d2bba47bedbd8446c1fd5cfae04d
EMMANUEL-WEB-APP <ndubuechi8@gmail.com>
2026-08-15 05:18:37 +0100

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_mail import Mail, Message
 
 app = Flask(__name__)
-
+app.secret_key = 'eleco-secret-key-2026a'
 # --- DATABASE CONFIG (Absolute path for Render & local compatibility) ---
 basedir = os.path.abspath(os.path.dirname(__file__))
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'election.db')
